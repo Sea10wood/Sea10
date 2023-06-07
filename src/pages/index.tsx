b626cf075c91cd897c7a1249ea6caace10f1c770
Sea10wood <miyusea5@gmail.com>
2023-06-07 12:51:57 +0900

    レスポンシブ対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,39 @@
 import Image from "next/image";
 import useWindowSize from "../hooks/Windowsize";
 
-import Footer from "../components/Footer";
+import { Button } from "@mui/material";
+import Link from "next/link";
 
 export default function Home() {
     const [width, height] = useWindowSize();
     return (
         <>
-            <Footer />
             <Image
-                src="/smartphone.gif"
-                alt="smartphone"
+                src="/sea10back.png"
+                alt="sea10back"
                 width={width}
                 height={height}
             />
+
+            <div
+                style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <Link href="/smartphone">
+                    <Button>スマートフォンの方はこちら</Button>
+                </Link>
+                <Link href="/smartphone">
+                    <Button>PC・タブレットの方はこちら</Button>
+                </Link>
+            </div>
         </>
     );
 }
